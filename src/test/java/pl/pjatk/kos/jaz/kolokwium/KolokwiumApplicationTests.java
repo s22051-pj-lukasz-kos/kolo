96b43828a572e784e38d78f6135b91642753e449
s22051-pj-lukasz-kos <s22051@pjwstk.edu.pl>
2022-07-03 14:08:06 +0200

    Initial commit

---
+++ src/test/java/pl/pjatk/kos/jaz/kolokwium/KolokwiumApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.kos.jaz.kolokwium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KolokwiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
